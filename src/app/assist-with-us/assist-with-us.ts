ffa7ac058e765d257bbf487e49bb916ed578b214
Hami\m.gohari <m.gohari@hami.ir>
2026-07-08 15:02:52 +0330

    #05-04-17

---
+++ src/app/assist-with-us/assist-with-us.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-assist-with-us',
+  imports: [],
+  templateUrl: './assist-with-us.html',
+  styleUrl: './assist-with-us.scss',
+})
+export class AssistWithUs {}
---
+++ src/app/weight-calculation/weight-calculation.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-weight-calculation',
+  imports: [],
+  templateUrl: './weight-calculation.html',
+  styleUrl: './weight-calculation.scss',
+})
+export class WeightCalculation {}
