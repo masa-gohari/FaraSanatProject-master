6f1434460f8882b1676fea4a05b694ded4b07880
Hami\m.gohari <m.gohari@hami.ir>
2026-08-18 16:16:58 +0330

    05-05-27

--- src/app/assist-with-us/assist-with-us.ts
+++ src/app/assist-with-us/assist-with-us.ts
@@ -1,10 +1,95 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { PageBanner } from '../shared/page-banner/page-banner';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+import { FormGroup, FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
+import { ContactMessage } from '../model/contact.model';
+import { ContactService } from '../services/contact.service';
 
 @Component({
   selector: 'app-assist-with-us',
-  imports: [PageBanner],
+  imports: [PageBanner, MatSnackBarModule, MatFormFieldModule, MatInputModule, MatRadioModule, ReactiveFormsModule,],
   templateUrl: './assist-with-us.html',
   styleUrl: './assist-with-us.scss',
 })
-export class AssistWithUs {}
+export class AssistWithUs {
+  private contactService = inject(ContactService);
+  form!: FormGroup;
+  contactModel!: ContactMessage;
+  users = [
+    { label: 'تولید کننده', value: 'producer' },
+    { label: 'مصرف کننده', value: 'consumer' }
+  ];
+
+  constructor(private fb: FormBuilder, private snackBar: MatSnackBar) {
+    this.buildFrom()
+  }
+
+  buildFrom() {
+    this.form = this.fb.group({
+      fullName: ['', Validators.required],
+      mobile: ['', [Validators.required, Validators.pattern(/^09\d{9}$/)]],
+      userType: ['consumer', Validators.required],
+    });
+  }
+
+  get f() { return this.form.controls }
+
+  submit() {
+    const formValues = this.form.getRawValue();
+
+    const fullNameValue = (formValues.fullName ?? '').trim();
+    const mobileValue = (formValues.mobile ?? '').trim();
+    const userType = formValues.userType;
+
+    if (!fullNameValue) {
+      this.snackBar.open('نام و نام‌خانوادگی را وارد نمایید.', '', {
+        duration: 3000,
+        horizontalPosition: 'left',
+        verticalPosition: 'bottom',
+        panelClass: ['error-snackbar']
+      });
+      return;
+    }
+
+    if (!mobileValue) {
+      this.snackBar.open('شماره موبایل خود را وارد نمایید.', '', {
+        duration: 3000,
+        horizontalPosition: 'left',
+        verticalPosition: 'bottom',
+        panelClass: ['error-snackbar']
+      });
+      return;
+    }
+
+    if (userType !== 'seller' && userType !== 'buyer') {
+      this.snackBar.open('نوع کاربری را انتخاب نمایید.', '', {
+        duration: 3000,
+        horizontalPosition: 'left',
+        verticalPosition: 'bottom',
+        panelClass: ['error-snackbar']
+      });
+      return;
+    }
+
+    this.contactModel = {
+      fullName: fullNameValue,
+      mobile: mobileValue,
+      userType: userType
+    };
+
+    console.log(this.contactModel);
+
+    this.contactService.sendMessage(this.contactModel).subscribe((q: any) => {
+      if (q.isSuccess) {
+        // پیام موفقیت
+      } else {
+        // پیام خطا
+      }
+    });
+  }
+
+
+}
--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,115 +1,34 @@
+import { routes } from './../app.routes';
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { TgjuIndicator, TgjuService } from '../services/tgju.service';
 import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { ProductService } from '../services/product.service';
-
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { ContactMessage } from '../model/contact.model';
-import { ContactService } from '../services/contact.service';
-import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatRadioModule } from '@angular/material/radio';
 import { Footer } from '../footer/footer';
+import { AssistWithUs } from '../assist-with-us/assist-with-us';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, RouterLink, Footer, ReactiveFormsModule, MatSnackBarModule, MatFormFieldModule, MatInputModule, MatRadioModule],
+  imports: [CommonModule, RouterLink, Footer, AssistWithUs],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
 
 export class Home implements OnInit {
   private tgjuService = inject(TgjuService);
-  private contactService = inject(ContactService);
   private productService = inject(ProductService);
+  private router = inject(Router);
   products = this.productService.products;
   indicators = signal<TgjuIndicator[]>([]);
   isExpanded = signal<boolean>(false);
-  form!: FormGroup;
   linkUrl = 'https://www.tgju.org/';
-  contactModel!: ContactMessage;
-  users = [
-    { label: 'تولید کننده', value: 'producer' },
-    { label: 'مصرف کننده', value: 'consumer' }
-  ];
-
-  constructor(private fb: FormBuilder, private snackBar: MatSnackBar) {
-    this.buildFrom()
-  }
 
   ngOnInit(): void {
     this.tgjuService.getIndicators().subscribe(data => {
       this.indicators.set(data);
     });
   }
-
-  buildFrom() {
-    this.form = this.fb.group({
-      fullName: ['', Validators.required],
-      mobile: ['', [Validators.required, Validators.pattern(/^09\d{9}$/)]],
-      userType: ['consumer', Validators.required],
-    });
-  }
-
-  get f() { return this.form.controls }
-
-submit() {
-  const formValues = this.form.getRawValue();
-
-  const fullNameValue = (formValues.fullName ?? '').trim();
-  const mobileValue = (formValues.mobile ?? '').trim();
-  const userType = formValues.userType;
-
-  if (!fullNameValue) {
-    this.snackBar.open('نام و نام‌خانوادگی را وارد نمایید.', '', {
-      duration: 3000,
-      horizontalPosition: 'left',
-      verticalPosition: 'bottom',
-      panelClass: ['error-snackbar']
-    });
-    return;
-  }
-
-  if (!mobileValue) {
-    this.snackBar.open('شماره موبایل خود را وارد نمایید.', '', {
-      duration: 3000,
-      horizontalPosition: 'left',
-      verticalPosition: 'bottom',
-      panelClass: ['error-snackbar']
-    });
-    return;
-  }
-
-  if (userType !== 'seller' && userType !== 'buyer') {
-    this.snackBar.open('نوع کاربری را انتخاب نمایید.', '', {
-      duration: 3000,
-      horizontalPosition: 'left',
-      verticalPosition: 'bottom',
-      panelClass: ['error-snackbar']
-    });
-    return;
-  }
-
-  this.contactModel = {
-    fullName: fullNameValue,
-    mobile: mobileValue,
-    userType: userType
-  };
-
-  console.log(this.contactModel);
-
-  this.contactService.sendMessage(this.contactModel).subscribe((q: any) => {
-    if (q.isSuccess) {
-      // پیام موفقیت
-    } else {
-      // پیام خطا
-    }
-  });
-}
-
 
   expand() {
     this.isExpanded.set(true);
@@ -121,5 +40,9 @@ submit() {
 
   toggle() {
     this.isExpanded.update(val => !val);
+  }
+
+  goToWeightCalculation() {
+    this.router.navigate(['/weight-calculation']);
   }
 }
